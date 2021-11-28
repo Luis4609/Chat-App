24e025eb8298f99d059eab960b1b89feb48ecf05
Luis Monzon <60436830+Luis4609@users.noreply.github.com>
2021-11-28 23:58:59 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -118,7 +118,6 @@ function template_footer()
         </div>
       </footer>
         <!-- Bootstrap core JavaScript -->
-        <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
